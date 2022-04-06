224e17dbcf5c2ce99cfc8572855017e6a3c64dab
gasparl <lkcsgaspar@gmail.com>
2022-04-06 17:26:41 +0200

    began merging

--- src/client/sidebar-page/index.js
+++ src/client/sidebar-page/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TS_sidebar from './components/tsSidebar';
+import App from './components/App';
 import './index.css';
 
-ReactDOM.render(<TS_sidebar />, document.getElementById('index'));
+ReactDOM.render(<App />, document.getElementById('index'));

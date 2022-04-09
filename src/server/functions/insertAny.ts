76f969b6ab54818549c31a0b436e1b5384a84f92
gasparl <lkcsgaspar@gmail.com>
2022-04-09 19:14:42 +0200

    namerange overlap cleaned

--- src/server/functions/insertAny.ts
+++ src/server/functions/insertAny.ts
@@ -25,6 +25,9 @@ export const insertAny = (textToInsert, textName = null, range = null) => {
             DocumentApp.getUi().alert('Insertion omitted: A cursor placed in the text or a selected text is needed to indicate the position of the insertion.');
         } else {
             var elements = selection.getRangeElements();
+            if (range !== null) {
+                elements.length = 1;
+            }
             var replace = true;
             for (var i = 0; i < elements.length; i++) {
                 if (elements[i].isPartial()) {
@@ -32,35 +35,37 @@ export const insertAny = (textToInsert, textName = null, range = null) => {
                     var startIndex = elements[i].getStartOffset();
                     var endIndex = elements[i].getEndOffsetInclusive();
                     var text = tElement.getText().substring(startIndex, endIndex + 1);
-                    DocumentApp.getUi().alert(text);
                     if (replace) {
-                        tElement.insertText(endIndex + 1, textToInsert);
+                        tElement.insertText(endIndex + 1, 'x');
+                        tElement.deleteText(startIndex, endIndex);
+                        tElement.insertText(startIndex + 1, textToInsert);
                         if (rangeBuilder === null) {
                             rangeBuilder = doc.newRange();
-                            rangeBuilder.addElement(tElement, endIndex + 1, endIndex + 1 + textToInsert.length - 1);
+                            rangeBuilder.addElement(tElement, startIndex + 1, startIndex + 1 + textToInsert.length - 1);
                         }
                         replace = false;
+                        tElement.deleteText(startIndex, startIndex);
+                    } else {
+                        tElement.deleteText(startIndex, endIndex);
                     }
-                    tElement.deleteText(startIndex, endIndex);
-
                 } else {
                     var eElement: any = elements[i].getElement();
                     // if not specified as "any", throws type errors for some reason
                     if (replace && eElement.editAsText) {
                         eElement.clear().asText().setText(textToInsert);
                         replace = false;
-                        // if (rangeBuilder === null) {
-                        //     rangeBuilder = doc.newRange();
-                        //     rangeBuilder.addElement(eElement);
-                        // }
+                        if (rangeBuilder === null) {
+                            rangeBuilder = doc.newRange();
+                            rangeBuilder.addElement(eElement);
+                        }
                     } else {
                         if (replace && i === elements.length - 1) {
                             var parent = eElement.getParent();
                             parent[parent.insertText ? 'insertText' : 'insertParagraph'](parent.getChildIndex(eElement), textToInsert);
-                            // if (rangeBuilder === null) {
-                            //     rangeBuilder = doc.newRange();
-                            //     rangeBuilder.addElement(eElement);
-                            // }
+                            if (rangeBuilder === null) {
+                                rangeBuilder = doc.newRange();
+                                rangeBuilder.addElement(eElement);
+                            }
                             replace = false; //not really necessary since it's the last one
                         }
                         eElement.removeFromParent();

7d778ea28dee4dd7f90e6693e6573013a165d596
gasparl <lkcsgaspar@gmail.com>
2022-04-10 21:33:21 +0200

    insert table revised

--- src/server/functions/insertTable.ts
+++ src/server/functions/insertTable.ts
@@ -1,67 +1,164 @@
 import { Group } from "../../client/sidebar-page/classes/Group"
 import { formatValue } from './formatValue';
 
-const insertTable = async (name: string, groups?: Group[]) => {
-  Word.run(async (context) => {
+const doc = DocumentApp.getActiveDocument();
+
+/**
+ * From: https://stackoverflow.com/a/40675038/9593181
+ *
+ * Splits the contents of the paragraph (or list item) at the given position,
+ * producing two adjacent paragraphs (or list items). This function may be used
+ * to insert any kind of element at an arbitrary document position, but placing
+ * it immediately before the second paragraph (or list item).
+ *
+ * @param {Position} pos The position where the paragraph (or list item) should
+ *     be split. `pos.getElement()` should be either a Text, Paragraph or
+ *     ListItem object.
+ *
+ * @returns {ContainerElement} The second (newly created) Paragraph or ListItem
+ *     object.
+ *
+ */
+const splitParagraphAt = (pos) => {
+    var el = pos.getElement(), offset = pos.getOffset();
+
+    var inParagraph = (el.getType() == DocumentApp.ElementType.PARAGRAPH || el.getType() == DocumentApp.ElementType.LIST_ITEM);
+
+    if (!inParagraph && (el.getType() != DocumentApp.ElementType.TEXT)) {
+        throw new Error("Position must be inside text or paragraph.");
+    }
+
+    var par;
+    if (inParagraph) {
+        // in this case, `offset` is the number of child elements before this
+        // Position within the same container element
+        par = el;
+        if (offset == par.getNumChildren()) {
+            // we're at the end of the paragraph
+            return par.getParent().insertParagraph(
+                par.getParent().getChildIndex(par) + 1, "");
+        }
+        el = par.getChild(offset);
+    }
+    else {
+        par = el.getParent();
+
+        if (par == null || (par.getType() != DocumentApp.ElementType.PARAGRAPH && par.getType() != DocumentApp.ElementType.LIST_ITEM)) {
+            throw new Error("Parent of text is not a paragraph or a list item.");
+        }
+    }
+
+    var parContainer = par.getParent();
+
+    if (!("insertParagraph" in parContainer)) {
+        throw new Error("Cannot insert another paragraph in this container.");
+    }
+
+    // This assumes the given position is in the current document.
+    // alternatively, one may traverse through parents of par until document
+    // root is reached.
+    //var doc = DocumentApp.getActiveDocument();
+
+    var elIndex = par.getChildIndex(el);
+    var newPar = par.copy();
+
+    var newEl = newPar.getChild(elIndex);
+
+    // remove everything up to position from the new element
+    if (!inParagraph && (offset != 0)) {
+        newEl.deleteText(0, offset - 1);
+    }
+    newEl = newEl.getPreviousSibling();
+    while (newEl != null) {
+        // get the previous sibling before we remove the element.
+        var prevEl = newEl.getPreviousSibling();
+        newEl.removeFromParent();
+        newEl = prevEl;
+    }
+
+    // since we might remove el itself, we get the next sibling here already
+    var nextEl = el.getNextSibling();
+
+    // remove everything from position onwards in the original element
+    if (!inParagraph && (offset != 0)) {
+        el.deleteText(offset, el.getText().length - 1);
+    }
+    else {
+        // we're at the beginning of the text (or just before a paragraph
+        // subelement) and need to remove the entire text/subelement.
+        el.removeFromParent();
+    }
+
+    el = nextEl;
+    while (el != null) {
+        // get the next sibling before we remove the element.
+        nextEl = el.getNextSibling();
+        el.removeFromParent();
+        el = nextEl;
+    }
+
+    // actually insert the newly created paragraph into the document tree.
+    switch (par.getType()) {
+        case DocumentApp.ElementType.PARAGRAPH:
+            parContainer.insertParagraph(parContainer.getChildIndex(par) + 1, newPar);
+            break;
+        case DocumentApp.ElementType.LIST_ITEM:
+            parContainer.insertListItem(parContainer.getChildIndex(par) + 1, newPar);
+            break;
+    }
+    return newPar;
+}
+
+const insertValue = (table, rownum: number, colnum: number, value, tag = null) => {
+    doc.setCursor(doc.newPosition(table.getCell(rownum, colnum), 0));
+    const cursor = doc.getCursor();
+    const cElement = cursor.insertText(value);
+    if (tag !== null) {
+        const rangeBuilder = doc.newRange();
+        rangeBuilder.addElement(cElement);
+        doc.addNamedRange(tag, rangeBuilder.build());
+    }
+    return (cElement)
+}
+
+
+const insertTable = (name: string, groups?: Group[]) => {
+    const cursor = doc.getCursor();
+
+    // Make sure cursor is found
     // Make sure there are groups and that there are statistics
-    if (groups && groups[0].statistics) {
-      const rows = groups.length + 1
-      const columns = groups[0].statistics?.length + 1
+    if (!cursor) {
+        DocumentApp.getUi().alert('Could not insert table; the cursor should indicate the position.');
+    } else if (groups && groups[0].statistics) {
+        const rows = groups.length + 1
+        const columns = groups[0].statistics ?.length + 1
 
-      const range = context.document.getSelection()
-      const table = range.insertTable(rows, columns, Word.InsertLocation.after)
+        const el = (cursor.getOffset() == 0 ? cursor.getElement() : splitParagraphAt(cursor));
+        const parentEl = el.getParent();
+        const table = parentEl.insertTable(parentEl.getChildIndex(el), Array(rows).fill(Array(columns).fill('')));
 
-      // Some styling
-      table.getBorder("All").type = "None"
-      table.getBorder("Top").type = "Double"
-      table.getBorder("Bottom").type = "Single"
+        // // Some styling
+        // table.getBorder("All").type = "None"
+        // table.getBorder("Top").type = "Double"
+        // table.getBorder("Bottom").type = "Single"
+        // Note: presently, such border styling seems not possible with GAS
 
-      // Set the first cell's content to the group name
-      const cellName = table.getCell(0, 0)
-      cellName.getBorder("Bottom").type = "Single"
-      cellName.body.getRange().insertText(name, Word.InsertLocation.replace)
+        // Set the first cell's content to the group name
+        insertValue(table, 0, 0, name)
 
-      // Set the content of the remaining cells in the first row to the names of the statistics
-      groups[0].statistics.forEach((statistic, i) => {
-        const cellStatisticsName = table.getCell(0, i + 1)
-        cellStatisticsName.getBorder("Bottom").type = "Single"
-        cellStatisticsName.body.getRange().font.italic = true
-        cellStatisticsName.body
-          .getRange()
-          .insertText(
-            statistic.symbol ? statistic.symbol : statistic.name,
-            Word.InsertLocation.replace
-          )
-      })
-
-      // Loop over each group and set the name and values
-      groups.forEach((group, i) => {
-        table
-          .getCell(i + 1, 0)
-          .body.getRange()
-          .insertText(group.name, Word.InsertLocation.replace)
-
-        group.statistics?.forEach((statistic, j) => {
-          const value = table
-            .getCell(i + 1, j + 1)
-            .body.getRange()
-            .insertContentControl()
-          value.tag = statistic.identifier
-          value.insertText(
-            formatValue(statistic, 2),
-            Word.InsertLocation.replace
-          )
+        // Set the content of the remaining cells in the first row to the names of the statistics
+        groups[0].statistics.forEach((statistic, i) => {
+            insertValue(table, 0, i + 1, statistic.symbol ? statistic.symbol : statistic.name).setItalic(true);
         })
-      })
 
-      return context.sync
+        // Loop over each group and set the name and values
+        groups.forEach((group, i) => {
+            insertValue(table, i + 1, 0, group.name)
+            group.statistics ?.forEach((statistic, j) => {
+                insertValue(table, i + 1, j + 1, formatValue(statistic, 2), statistic.identifier)
+            })
+        })
     }
-  }).catch(function (error) {
-    console.log("Error: " + error)
-    if (error instanceof OfficeExtension.Error) {
-      console.log("Debug info: " + JSON.stringify(error.debugInfo))
-    }
-  })
 }
 
 export { insertTable }

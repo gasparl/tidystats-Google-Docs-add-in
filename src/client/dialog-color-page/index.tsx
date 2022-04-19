404553d07985dc97b10a3d7db4d143c8da74ac02
gasparl <lkcsgaspar@gmail.com>
2022-04-19 19:01:31 +0200

    renamed folder 2

---
+++ src/client/dialog-color-page/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DialogColor } from './components/DialogColor';
+import './index.css';
+
+ReactDOM.render(<DialogColor />, document.getElementById('index'));
